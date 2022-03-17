bfd00c15c9e6425a313844d6a6c32568a2d20fe4
pat0030 <48974531+pat0030@users.noreply.github.com>
2022-03-17 12:36:13 +1100

    Create start_window_creation.py

---
+++ start_window_creation.py
@@ -0,0 +1,13 @@
+# imports
+from email import message
+from tkinter import *
+from tkinter import ttk, messagebox
+from tkinter.messagebox import askokcancel
+
+# making a root window
+root = Tk()
+frame_header = ttk.Frame(root)
+frame_header.pack()
+
+mainloop()
+
